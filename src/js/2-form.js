c8a4cc4aa50046156f5ec64ed135a73c70fea4f7
Yaroslava <yaroslava.hovenko@gmail.com>
2024-01-17 21:41:17 +0200

    add from

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,8 +1,40 @@
 const form = document.querySelector('.feedback-form');
-const KEY_LC = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 
+form.addEventListener('submit', onSubmit);
 form.addEventListener('input', onInput);
 
-function onInput(evt) {
-  //
+const email = form.elements.email.value.trim();
+const message = form.elements.message.value.trim();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+
+  if (!email || !message) {
+    alert('Please, fill in all fields before sending!');
+    return;
+  }
+
+  localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
 }
+
+function onInput() {
+  const value = {
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(value));
+}
+
+function saveInfoFromStorage() {
+  const info = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+  form.elements.email.value = info.email || '';
+  form.elements.message.value = info.message || '';
+}
+
+saveInfoFromStorage();
